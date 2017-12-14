6c879ae0434423ca7ddd9ada692f2a16b0ca35f8
alfin17 <32080540+alfin17@users.noreply.github.com>
2017-12-14 20:40:33 +0700

    Update index.php

--- SimpleLineBotPHP-remastered/index.php
+++ SimpleLineBotPHP-remastered/index.php
@@ -46,14 +46,43 @@ $app->post('/', function ($request, $response)
 	foreach ($data['events'] as $event)
 	{
 		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) == 'halo')
-		{
-			$message = "Halo juga";
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
+	if($userMessage == "Diskon"){
+$carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("TETRA SNEAKERS SHOES - BROWN TAN", "Harga Normal Rp. 469.000 Harga Diskon Rp. 329.000","https://www.mensrepublic.id/assets/images/uploads/product/tetra-brown-tan-1512971652-KtRJ81iImSYv.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/tetra-sneakers-shoes-brown-tan-79042"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ARTA - BROWN", "Harga Normal Rp. 289.000 Harga Diskon Rp. 229.000","https://www.mensrepublic.id/assets/images/uploads/product/arta-brown-1512968017-XeFfZ6Dg1ETK.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/arta-brown-209542"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ARTA - BLACK", "Harga Normal Rp. 289.000 Harga Diskon Rp. 229.000","https://www.mensrepublic.id/assets/images/uploads/product/arta-black-1512968000-l9Qt2OY41DOB.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/arta-black-209539"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ORA - BROWN", "Harga Normal Rp. 289.000 Harga Diskon Rp. 229.000","https://www.mensrepublic.id/assets/images/uploads/product/ora-brown-1512971491-hEYVd6EtS4NU.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/ora-brown-209536"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ORA - BLACK", "Harga Normal Rp. 289.000 Harga Diskon Rp. 229.000","https://www.mensrepublic.id/assets/images/uploads/product/ora-black-1512972560-LMEQIFe0FL1v.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/ora-black-209531"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("BRATAN - BROWN", "Harga Normal Rp. 469.000 Harga Diskon Rp. 329.000","https://www.mensrepublic.id/assets/images/uploads/product/bratan-brown-1512968583-BPQF0ipVhbjM.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/bratan-brown-209524"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("BRATAN - BLACK", "Harga Normal Rp. 469.000 Harga Diskon Rp. 329.000","https://www.mensrepublic.id/assets/images/uploads/product/bratan-black-1512968548-eXeE0N6cSp9L.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/bratan-black-209512"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ALHENA SNEAKERS SHOES - CAMEL", "Harga Normal Rp. 195.000 Harga Diskon Rp. 185.000","https://www.mensrepublic.id/assets/images/uploads/product/alhena-camel-1512786588-qFNDNLWpx8i9.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/alhena-sneakers-shoes-camel-125027"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ALHENA SNEAKERS SHOES - BLACK", "Harga Normal Rp. 195.000 Harga Diskon Rp. 185.000","https://www.mensrepublic.id/assets/images/uploads/product/alhena-black-1512786540-uqv0dBlb0Jeq.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/alhena-sneakers-shoes-black-125006"),
+  ]),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("ALHENA SNEAKERS SHOES - GREY", "Harga Normal Rp. 195.000 Harga Diskon Rp. 185.000","https://www.mensrepublic.id/assets/images/uploads/product/alhena-grey-1512786676-CGlXbhnVtJVE.jpg",[
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Pesan Sekarang',"http://vicious.id/alhena-sneakers-shoes-grey-78176"),
+  ]),
+  ]);
+$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('Diskon Up To 50%',$carouselTemplateBuilder);
+$result = $bot->replyMessage($event['replyToken'], $templateMessage);
+return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+}
 	}
 	
 
